47f816f5f27c1d9697816140eaad09f3d2e8af85
camclark02 <164952709+camclark02@users.noreply.github.com>
2024-04-21 19:50:10 +0000

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   get("/", { :controller => "zebra", :action => "home_page" })
 
-  get("/dice/2/6", { :controller => "zebra", :action => "2d6" })
+  get("/dice/2/6", { :controller => "zebra", :action => "two_six" })
 
-  get("/dice/2/10", { :controller => "zebra", :action => "2d10" })
+  get("/dice/2/10", { :controller => "zebra", :action => "two_ten" })
 
-  get("/dice/1/20", { :controller => "zebra", :action => "1d20" })
+  get("/dice/1/20", { :controller => "zebra", :action => "one_twenty" })
 
-  get("/dice/5/4", { :controller => "zebra", :action => "5d4" })
-  
+  get("/dice/5/4", { :controller => "zebra", :action => "five_four" })
+
   get("/dice/:alice/:bob", { :controller => "zebra", :action => "dynamic" })
 end
